f1a3839d84b73829b41027da5858ed0c23d9dda1
Miotis2004 <miotis2004@gmail.com>
2022-01-13 11:43:06 -0800

    First Commit

---
+++ SocMedia/SocMedia/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SocMedia
+//
+//  Created by Ronald Joubert on 1/13/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SocMedia/SocMedia/SocMediaApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocMediaApp.swift
+//  SocMedia
+//
+//  Created by Ronald Joubert on 1/13/22.
+//
+
+import SwiftUI
+
+@main
+struct SocMediaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
